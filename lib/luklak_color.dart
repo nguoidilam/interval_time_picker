3c3c41c88068f7c2feb7af40ca2cfbb1a797fc6a
QuangLM <quanglm@hopluc.net>
2023-11-15 11:58:44 +0700

    custom ui

---
+++ lib/luklak_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class LukLakColor{
+  static const Color primaryColor = Color(0xff1242CA);
+  static const Color textSecondary = Color(0xff737985);
+  static const Color textPrimary = Color(0xff172B4D);
+}
